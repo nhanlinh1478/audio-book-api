62e9b5d4aeb5a66144fe8097ffb1fa6755488ddd
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2022-06-11 12:45:56 +0700

    add Dockerfile, docker-compose.yml

--- src/webAPI/controllers/index.controller.js
+++ src/webAPI/controllers/index.controller.js
@@ -1,21 +1,19 @@
-const AuthService = require("../../applicationCore/services/auth.service");
+const AuthService = require('../../applicationCore/services/auth.service');
 class IndexController {
-  async index(req, res) {
-    const authService = new AuthService();
-    const email = "admin@admin.com";
-    // password = admin@admin.com
-    const password =
-      "$2b$10$LZYskePik4Xa1ZBC31yJR./dUW1bd6SKJeeqnMj0ccSUyM7KIyFtO";
-    
-    const _message = await authService.CreateSuperAdmin(email, password, code);
+    async index(req, res) {
+        const authService = new AuthService();
+        const email = 'admin@admin.com';
+        // password = admin@admin.com
+        const password = '$2b$10$LZYskePik4Xa1ZBC31yJR./dUW1bd6SKJeeqnMj0ccSUyM7KIyFtO';
 
-    return res.json({
-      code,
-      success: false,
-      message: "Route not found. Please check the URL and try again.",
-      _message: JSON.parse(_message),
-    });
-  }
+        const _message = await authService.CreateSuperAdmin(email, password);
+
+        return res.json({
+            success: false,
+            message: 'Route not found. Please check the URL and try again.',
+            _message: JSON.parse(_message),
+        });
+    }
 }
 
 module.exports = new IndexController();
